5b6aeb2d45f1027848342657994e8ebc366e846e
laszlogal1272008-create <laszlogal1272008@gmail.com>
2025-10-16 06:14:30 +0100

    Fix case sensitivity: rename information.jsx to Information.jsx

---
+++ src/components/Information.jsx
@@ -0,0 +1,23 @@
+import React from 'react';import React from 'react';
+
+
+
+function Information() {function Information() {
+
+  return (  return (
+
+    <div className="information">    <div className="information">
+
+      <h1>Information</h1>      <h1>Information</h1>
+
+      <p>This is your information page.</p>      <p>This is your information page.</p>
+
+    </div>    </div>
+
+  );  );
+
+}}
+
+
+
+export default Information;export default Information;
