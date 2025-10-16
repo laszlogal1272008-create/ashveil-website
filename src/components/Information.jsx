a4f515ea8e70aca7b8e280d1058ab583353db83c
laszlogal1272008-create <laszlogal1272008@gmail.com>
2025-10-16 06:19:22 +0100

    Fix duplicate React import in Information.jsx

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,23 +1,12 @@
-import React from 'react';import React from 'react';
+import React from 'react';
 
+function Information() {
+  return (
+    <div className="information">
+      <h1>Information</h1>
+      <p>This is your information page.</p>
+    </div>
+  );
+}
 
-
-function Information() {function Information() {
-
-  return (  return (
-
-    <div className="information">    <div className="information">
-
-      <h1>Information</h1>      <h1>Information</h1>
-
-      <p>This is your information page.</p>      <p>This is your information page.</p>
-
-    </div>    </div>
-
-  );  );
-
-}}
-
-
-
-export default Information;export default Information;
+export default Information;
